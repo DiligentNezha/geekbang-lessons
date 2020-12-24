93dab66a5bf7665ed47e5411d584fae34f93fe27
jingkaihui <kaihuijing@gmail>
2020-12-24 12:45:30 +0800

    aop verity

---
+++ thinking-in-spring/aop-overview/src/main/java/org/geekbang/thinking/in/spring/aop/verify/EchoProxyService.java
@@ -0,0 +1,24 @@
+package org.geekbang.thinking.in.spring.aop.verify;
+
+/**
+ * @author <a href="kaihuijing@gmail.com">jingkaihui</a>
+ * @description
+ * @date 2020/12/20
+ */
+public class EchoProxyService implements EchoService{
+
+    private EchoService echoService;
+
+    public EchoProxyService(EchoService echoService) {
+        this.echoService = echoService;
+    }
+
+    @Override
+    public String echo(String msg) {
+        long start = System.currentTimeMillis();
+        String result = echoService.echo(msg);
+        long costTime = System.currentTimeMillis() - start;
+        System.out.println("echo 方法执行时间: " + costTime + " ms.");
+        return result;
+    }
+}
---
+++ thinking-in-spring/aop-overview/src/main/java/org/geekbang/thinking/in/spring/aop/verify/StaticProxyDemo.java
@@ -0,0 +1,12 @@
+package org.geekbang.thinking.in.spring.aop.verify;
+
+/**
+ * @author <a href="kaihuijing@gmail.com">jingkaihui</a>
+ * @description
+ * @date 2020/12/20
+ */
+public class StaticProxyDemo {
+    public static void main(String[] args) {
+        new EchoProxyService(new DefaultEchoService()).echo("good");
+    }
+}
